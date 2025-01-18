8452c3c8af0f06a530985e088bdeb526e750d55a
Skyler Mäntysaari <sm+git@skym.fi>
2025-01-18 14:14:10 +0200

    the default arch can be arm64 for now..

--- internal/artifacts/versions.go
+++ internal/artifacts/versions.go
@@ -153,7 +153,7 @@ func (m *Manager) fetchOfficialExtensions(tag string) error {
 func (m *Manager) fetchOfficialOverlays(ctx context.Context, tag string) error {
 	var overlays []OverlayRef
 
-	if err := m.fetchImageByTag(OverlayManifestImage, tag, "", imageExportHandler(func(_ *zap.Logger, r io.Reader) error {
+	if err := m.fetchImageByTag(OverlayManifestImage, tag, ArchArm64, imageExportHandler(func(_ *zap.Logger, r io.Reader) error {
 		var extractErr error
 		overlays, extractErr = extractOverlayList(r)
 
